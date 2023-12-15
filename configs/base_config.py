c0d5e1378410dbe8dc1fdf59cdfc2387fa6f62b2
hubo0417 <30993479@qq.com>
2023-12-15 16:10:46 +0800

    配置调整

--- configs/base_config.py
+++ configs/base_config.py
@@ -15,17 +15,19 @@ BASE_CONFIG = {
     # sdxl的lora模型文件地址
     "sdxl_lora_path": "D:\\ChatGLM2-6B\\stable-diffusion-xl-base-1.0\\lora",
     # 百度翻译的APPID
-    "baidu_appid": "20231025001858786",
+    "baidu_appid": "",
     # 百度翻译的APPKEY
-    "baidu_app_key": "LazJH8nmTXw5YNND_QO4"
+    "baidu_app_key": "",
+    # blip模型地址
+    "blip_model_path": "D:\\EasyGC\\blip"
 }
 # base模型相关的vae文件地址
 VAE_PATH = f"{BASE_CONFIG['sdxl_base_path']}\\vae_fix"
 # 图片生成之后的保存地址
 BASE_FILE_PATH = "D:\\ChatGLM2-6B\\knowledge_center"
-# 谷歌搜索引擎的key
+# 谷歌搜索引擎APIKey
 GOOGLE_APIKEY = ""
-# 谷歌搜索引擎的自有ID
+# 谷歌搜索引擎资源ID
 GOOGLE_SEARCH_ID = ""
 # Azure OpenAI服务的key
 AZURE_OPENAI_KEY = ""
